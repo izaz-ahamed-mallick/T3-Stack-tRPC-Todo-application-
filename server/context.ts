ecd352e2be4f451ccfcbf7c9550d608129cb34df
Izaz Ahamed Mallick <izazahamedmallick98@gmail.com>
2025-02-09 00:25:35 +0530

    prisma setup complete

--- server/context.ts
+++ server/context.ts
@@ -1,9 +1,11 @@
+import { prisma } from "@/prisma/prismaClient";
+
 /**
  * Context Factory Function
  * Runs on every request to generate a new context object.
  */
 export const createContext = async () => {
-    return {}; // Return an empty object for now (you can add auth, db, etc.)
+    return { prisma }; // Return an empty object for now (you can add auth, db, etc.)
 };
 
 // Infer the context type for tRPC
